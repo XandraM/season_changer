cbf8096cec94e3c332f49c85c46676e24fcaa91b
Alexandra Matisová <alexandra.matisova@student.tuke.sk>
2025-12-12 16:21:09 +0100

    Initial commit

---
+++ js/season_changer.js
@@ -0,0 +1,26 @@
+const client = new Paho.Client("openlab.kpi.fei.tuke.sk", 443, "player" + Date.now());
+client.connect({onSuccess: onConnect, reconnect: true, useSSL: true, keepAliveInterval: 10, timeout: 10});
+
+function onConnect() {
+    console.log("Connected to MQTT broker");
+    client.subscribe("openlab/voice/recognition");
+}
+
+client.onMessageArrived = function (message) {
+    const text = message.payloadString.trim().toLowerCase();
+    console.log("Received:", text);
+
+    const seasons = ["jar", "leto", "jesen", "zima"];
+
+    if (seasons.includes(text)) {
+        showOnlySeason(text);
+    }
+};
+
+function showOnlySeason(seasonId) {
+    // Skryjeme vsetky stranky
+    document.querySelectorAll("body>div").forEach(elem => elem.classList.add("hidden"));
+    // Zobrazime zvolenu stranku
+    document.getElementById(seasonId).classList.remove("hidden");
+
+}
